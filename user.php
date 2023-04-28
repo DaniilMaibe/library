2533a68a68be82fb78d4462d05e20c561185d5ec
Daniil_Maibe <daniilmaibe@gmail.com>
2023-04-28 13:40:28 +0400

    выводится последняя отсканированная книга

--- user.php
+++ user.php
@@ -1,7 +1,20 @@
 <?php
 session_start();
+include("db_connect.php");
 if (isset($_SESSION['user'])) {
-    echo("Вы вошли как " . $_SESSION['user']['nick']);
+    echo("Вы вошли как " . $_SESSION['user']['nick']).'<br>';
 } else {
     echo("Вы не авторизированы.");
+}
+
+// "SELECT * FROM `history` ORDER BY time DESC LIMIT 1"
+
+  
+$resultScan = mysqli_query($db, "SELECT * FROM `history` ORDER BY time DESC LIMIT 1");
+if (mysqli_num_rows($resultScan) > 0){
+    while($rowData = mysqli_fetch_assoc($resultScan)){
+        echo 'Последняя отсканированная книга: '.$rowData["uid"].'<br>';
+    }
+}else {
+    echo("Ошибка");
 }
