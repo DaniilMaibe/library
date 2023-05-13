f15b98edb7531232a698e5c5a759ad675961ba7b
Daniil_Maibe <daniilmaibe@gmail.com>
2023-05-13 12:25:09 +0300

    изменения от Ани

--- user.php
+++ user.php
@@ -1,63 +1,126 @@
 <?php
 session_start();
 include("db_connect.php");
-if (isset($_SESSION['user'])) {
-    echo("Вы вошли как " . $_SESSION['user']['nick']).'<br>';
-    $currentUser = $_SESSION['user']['nick'];
-} else {
-    echo("Вы не авторизированы.");
-}
-
-
-echo '<b>Список взятых книг: </b>'.'<br>';
-echo '<table bordercolor="black" border="1" width="30%">';
-$resultAllBooks = mysqli_query($db, "SELECT * FROM `rfid_uid_area` WHERE pid = '{$currentUser}'");
-if (mysqli_num_rows($resultAllBooks) > 0){
-    while($rowDataA = mysqli_fetch_assoc($resultAllBooks)){
-        // echo 'UID: '.$rowDataA["uid"].'<br>';
-        $currentUID = $rowDataA["uid"];
-        $resultAllBooksName = mysqli_query($db, "SELECT * FROM `rfid_uid_name` WHERE uid = '{$currentUID}'");
-        $rowDataAN = mysqli_fetch_assoc($resultAllBooksName);
-        if ($rowDataAN == null){
-            echo 'Ошибка. Такой книги нет в базе данных'.'<br>';
-        } else {
-            // echo 'Название: '.$rowDataAN["name"].'<br>';
-            echo '<tr>';
-            echo '<td>'.$rowDataAN["name"].'</td>';
-            echo '<td>'.$rowDataA["date_take"].'</td>';
-            // для вычисления дедлайна добавляем две недели
-            $newDate = new DateTime($rowDataA["date_take"]);
-            $newDate->add(new DateInterval('P14D')); // P14D means a period of 14 days
-            $deadline = $newDate->format('Y-m-d');
-            echo '<td>'.$deadline.'</td>';
-            echo '</tr>';
-        }
-        echo '<br>';
-    }
-}else {
-    echo("У Вас не обнаружено ни одной взятой книги");
-}
-echo '</table>';
 
 
 ?>
 
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Меню</title>
+    <title>Личный кабинет</title>
+    <link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    
 </head>
-<body>
-<?php 
-echo '<h3><a href="scan_take.php" class="button beer-button-blue">ВЗЯТЬ КНИГУ</a></h3>';
-echo '<h3><a href="scan_return.php" class="button beer-button-blue">ВЕРНУТЬ КНИГУ</a></h3>';
-echo '<h3><a href="exit.php" class="button beer-button-blue">ВЫЙТИ</a></h3>';
-?>
+<body class="container">
 
-</form>
+    <header class="navbar navbar-expand-lg navbar-light bg-white fixed-top py-3" id="mainNav">
+        <div class="container"> 
+            <a class="navbar-brand js-scroll-trigger" href="index.html" style="font-weight: bold;"><img class="logo" src="img/logo.png" alt="3D MIEM">БИБЛИОТЕКА МИЭМ</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto" id="menu">
+                    <li class="nav-item"> <a class="nav-link js-scroll-trigger" href="index.html" >Главная</a> </li>
+                    <li class="nav-item"> <a class="nav-link js-scroll-trigger" href="manual.html">Инструкция</a> </li>
+                    <li class="nav-item"> <a class="nav-link js-scroll-trigger" href="user.php" style="font-weight:bold;">Личный кабинет</a> </li>
+                    <li class="nav-item"> <a class="nav-link js-scroll-trigger" href="exit.php">Выйти</a> </li>
+                </ul>
+            </div>
+        </div>
+    </header>
+
+    <div class="row" style="padding-top: 80px;">
+        <div class="col-6">
+        <?php
+            if (isset($_SESSION['user'])) {
+                $currentUser = $_SESSION['user']['nick'];
+                $resultAllUser = mysqli_query($db, "SELECT * FROM `person_info` WHERE phone = '{$currentUser}'");
+                $rowDataAU = mysqli_fetch_assoc($resultAllUser);?>
+
+                <div style="font-weight: 600; font-size: 30px"><?=$rowDataAU["last_name"]?> <?=$rowDataAU["first_name"]?> <?=$rowDataAU["patronymic"]?></div>
+                <div style="font-size: 18px"><?=$rowDataAU["phone"]?></div>
+                <div style="font-size: 18px"><?=$rowDataAU["email"]?></div>
+
+            <?php } else {
+                echo("Вы не авторизированы.");
+            }
+        ?>
+        </div>
+        <div class="col-6">
+            <div class="row" style="margin-top: 30px; ">
+                <div class="mx-auto">
+                    <a class="button_user"  href="scan_take.html" role="button">Взять книгу</a>
+                </div>
+            </div>
+
+            <div class="row" style="margin-top: 30px;">
+                <div class="mx-auto">
+                    <a class="button_user" href="scan_return.html" role="button">Вернуть книгу</a>
+                </div>
+            </div>
+        </div>
+        
+
+        
+    </div>
+    <div class="row">
+        
+        
+    </div>
+    <div class="row">
+        <h2>Список взятых книг: </h2>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Автор</th>
+                <th scope="col">Название</th>
+                <th scope="col">Дата взятия</th>
+                <th scope="col">Дата возврата </th>
+                </tr>
+            </thead>
+            <tbody>
+        <?php
+            $resultAllBooks = mysqli_query($db, "SELECT * FROM `rfid_uid_area` AS rua 
+            LEFT JOIN `rfid_uid_name` AS run ON rua.uid = run.uid
+            WHERE pid = '{$currentUser}'");
+            //$rowDataA = mysqli_fetch_assoc($resultAllBooks);
+            foreach ($resultAllBooks as $f) {
+
+                $newDate = new DateTime($f["date_take"]);
+                $newDate->add(new DateInterval('P14D')); // P14D means a period of 14 days
+                $deadline = $newDate->format('d-m-Y');
+            
+                ?>
+                <tr>
+                    <td><?=$f['uid'] ?></td>
+                    <td><?=$f['author'] ?></td>
+                    <td><?=$f['name'] ?></td>
+                    <td><?=$f['date_take'] ?></td>
+                    <td><?=$deadline?></td>
+                </tr>
+                
+    <?php }?>  
+
+
+
+             
+            </tbody>
+        </table>
+                        
+                        
+    </div>
+    
+    
+
+    
 </body>
 </html>
 
